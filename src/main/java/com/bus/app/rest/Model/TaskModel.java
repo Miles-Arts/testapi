34e61b431bf3d82603986b9552245bf6bd806f90
Milton Figueredo Miles <gratno@gmail.com>
2023-06-18 21:22:21 -0500

    Añadien funciones a Model  Contructores and Getters y Setters

--- src/main/java/com/bus/app/rest/Model/TaskModel.java
+++ src/main/java/com/bus/app/rest/Model/TaskModel.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @Entity
-@RestController
 public class TaskModel {
 
         @Id
@@ -24,4 +23,30 @@ public class TaskModel {
         private String titulo;
         private String descripcion;
 
+        public TaskModel() {
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getTitulo() {
+                return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+                this.titulo = titulo;
+        }
+
+        public String getDescripcion() {
+                return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+                this.descripcion = descripcion;
+        }
 }
